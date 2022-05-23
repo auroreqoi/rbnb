81d82486b6d9609c3b0553d8c9b46f45dce42c3d
Aurore Rigault <rigault.aurore@gmail.com>
2022-05-23 17:49:33 +0200

    beginning hives front & end

--- app/controllers/hives_controller.rb
+++ app/controllers/hives_controller.rb
@@ -1,13 +1,29 @@
 class HivesController < ApplicationController
   def new
+    @hive = Hive.new
   end
 
   def create
+    @hive = Hive.new(hive_params)
+    @hive.save
+    if @hive.save
+      redirect_to hive_path(@hive)
+    else
+      render :new
+    end
   end
 
   def show
+    @hive = Hive.find(params[:id])
   end
 
   def index
+    @hives = Hive.all
+  end
+
+  private
+
+  def hive_params
+    params.require(:hive).permit(:name, :description, :address, :price_per_day, :queen_id)
   end
 end

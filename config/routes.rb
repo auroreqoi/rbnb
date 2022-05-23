67ebfb7c41b53eecd575a583a53c80e3eda20219
Aurore Rigault <rigault.aurore@gmail.com>
2022-05-23 15:27:10 +0200

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :hives, only: [:index, :show, :new, :create]
+  resources :reservations, only: [:index, :new, :create] do
+    member do
+      patch :accept
+      patch :decline
+    end
+  end
+  namespace :queen do
+    resources :reservations, only: [:index]
+  end
 end

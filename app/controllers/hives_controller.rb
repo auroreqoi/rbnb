fcda5fd5410d3816e433f8ad44c6b2c523962cc9
Aurore Rigault <rigault.aurore@gmail.com>
2022-05-24 15:51:16 +0200

    indentation corrected

--- app/controllers/hives_controller.rb
+++ app/controllers/hives_controller.rb
@@ -6,16 +6,9 @@ class HivesController < ApplicationController
   end
 
   def show
-    @hive = Hive.find(params[:id])
   end
 
   def index
     @hives = Hive.all
-  end
-
-  private
-
-  def hive_params
-    params.require(:hive).permit(:name, :description, :address, :price_per_day, :queen_id)
   end
 end

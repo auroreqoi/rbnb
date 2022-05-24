e7e7017ecfdce757b0ef5e69634ce1b7be1b898e
Shiyun BATLLE <shiyun.rabbit@gmail.com>
2022-05-24 12:21:31 +0200

    backend of controller and view of new and creat of hive, front not yet

--- app/controllers/hives_controller.rb
+++ app/controllers/hives_controller.rb
@@ -1,13 +1,28 @@
 class HivesController < ApplicationController
   def new
+    @hive = Hive.new
   end
 
   def create
+    @hive = Hive.new(hive_params)
+    @hive.queen = current_user
+
+    if @hive.save
+      redirect_to hive_path(@hive)
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def index
+  end
+
+  private
+
+  def hive_params
+    params.require(:hive).permit(:name, :description, :address, :price_per_day)
   end
 end

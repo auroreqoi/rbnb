bb3a421947b54b4ff99f8583a96f83ba15f8fb85
Shiyun BATLLE <shiyun.rabbit@gmail.com>
2022-05-25 15:36:40 +0200

    Fixes

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/auroreqoi/cl3lgej9w002114mwmwkuz4c4"
+      style: "mapbox://styles/mapbox/streets-v10"
     })
 
     this.#addMarkersToMap()

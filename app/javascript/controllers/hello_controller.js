fbb160b87cd99f6aa3dc7f42e4d1102ac40e9811
Shiyun BATLLE <shiyun.rabbit@gmail.com>
2022-05-25 15:12:35 +0200

    fix card home

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,6 +1,6 @@
-// Visit The Stimulus Handbook for more details 
+// Visit The Stimulus Handbook for more details
 // https://stimulusjs.org/handbook/introduction
-// 
+//
 // This example controller works with specially annotated HTML like:
 //
 // <div data-controller="hello">
@@ -8,11 +8,10 @@
 // </div>
 
 import { Controller } from "stimulus"
+import flatpickr from "flatpickr";
 
 export default class extends Controller {
-  static targets = [ "output" ]
-
   connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+    flatpickr(".datepicker", {});
   }
 }
